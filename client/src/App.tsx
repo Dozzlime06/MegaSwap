a304a918ef5d16517efaec19b924d0c1bdb82e1d
HyperLeverage <backpack.exhange@gmail.com>
2025-12-30 10:55:59 +0800

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import SwapPage from "@/pages/swap";
+import AdminPage from "@/pages/admin";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={SwapPage} />
+      <Route path="/home" component={Home} />
+      <Route path="/0x315admin" component={AdminPage} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
